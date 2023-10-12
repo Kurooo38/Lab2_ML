58627ff74da57dfbe17300f3efaa52bb059fd16a
Kurooo38 <147548712+Kurooo38@users.noreply.github.com>
2023-10-12 22:18:50 +0700

    Update lab_2_ml.py

--- lab_2_ml.py
+++ lab_2_ml.py
@@ -48,12 +48,12 @@ ax.scatter([1, 2, 3], [1, 2, 3])
 X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
 mp.contourf(X1,X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
-            alpha = 0.75, cmap = ListedColormap(('red','green')))
+            alpha = 0.75, cmap = ListedColormap(['red','green']))
 mp.xlim(X1.min(), X1.max())
 mp.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
   mp.scatter(x_set[y_set == j, 0],x_set[y_set == j, 1],
-             c = ListedColormap(('red', 'green'))(i),label = j)
+             c = ListedColormap(['red','green'])(i),label = j)
 mp.title('Naive Bayes (Training set)')
 mp.xlabel('Age')
 mp.ylabel('Estimated Salary')
